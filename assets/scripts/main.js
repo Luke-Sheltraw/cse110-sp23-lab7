3c321485e7422ac83a25755d6e08dfccb6ec07dc
Luke-Sheltraw <lukesheltraw@gmail.com>
2023-05-20 17:17:29 -0700

    sw fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -39,13 +39,7 @@ function initializeServiceWorker() {
   window.addEventListener('load', async () => {
     try {
       const registration = await navigator.serviceWorker.register('./sw.js');
-      if (registration.installing) {
-        console.log("Service worker installing");
-      } else if (registration.waiting) {
-        console.log("Service worker installed");
-      } else if (registration.active) {
-        console.log("Service worker active");
-      }
+      console.log('Registration successful');
     } catch (error) {
       console.log(`Registration failed with ${error}`);
     }
